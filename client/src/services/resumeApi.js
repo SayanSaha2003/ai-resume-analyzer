9d9633f1bbecbe9d8efa0d670fa7022180698802
Sayan Saha <sayansaha555555@gmail.com>
2026-09-07 14:52:18 +0530

    Update client env for deploy

--- client/src/services/resumeApi.js
+++ client/src/services/resumeApi.js
@@ -1,5 +1,5 @@
 export const analyzeResume = async (resumeText) => {
-    const response = await fetch("http://localhost:5000/api/analyze", {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/analyze`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
